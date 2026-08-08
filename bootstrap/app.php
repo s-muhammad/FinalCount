23da90b24c8faea06039b897c6a6b2e1b931915b
Muhammad <seiedmohammadmohammadi@gmail.com>
2026-08-08 20:31:16 +0330

    fix app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,8 @@
 <?php
-
 use App\Http\Middleware\SetLanguage;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -16,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
             SetLanguage::class,
         ]);
         $middleware->trustProxies(at: '*');
-})
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

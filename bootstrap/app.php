c139695255619db9ed048fc58e60d07313201fdc
Muhammad <seiedmohammadmohammadi@gmail.com>
2026-08-08 20:13:50 +0330

    add proxy middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             SetLanguage::class,
         ]);
+        $middleware->trustProxies(at: '*');
+})
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

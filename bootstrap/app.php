9522fa0216caa5c52590a3e230999b5416474d6d
Muhammad <seiedmohammadmohammadi@gmail.com>
2026-09-06 11:20:07 +0330

    fix proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
